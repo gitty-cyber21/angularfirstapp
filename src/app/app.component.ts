32f539570685884dc2f0f4531691743e549b063c
gitty-cyber21 <c14180218@john.petra.ac.id>
2021-02-23 19:04:52 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  hasil = 0;
+  a=0;
+  b=0;
+
+  item1={
+    nama : "Jeruk", harga : 15000, satuan: "kg"
+  }
+
+  item2 = [
+    {nama : "Jeruk", harga : 15000, satuan: "kg"},
+    {nama : "Apel", harga : 5000, satuan: "kg"}
+  ]
+
+  tampil : boolean = false;
+
+  tulisanOPEN : String;
+  OPEN(){
+    this.tulisanOPEN = "toko DIBUKA"
+    alert("DIBUKA");
+  }
+
+  values="";
+  onKey(event: any){
+    this.values += event.target.value + '|';
+  }
+ 
 }
